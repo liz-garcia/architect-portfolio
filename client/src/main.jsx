75090f6be7862d3daf48700805e802732c6de475
liz-garcia <lizgarciamarin@gmail.com>
2024-08-23 18:28:02 +0200

    Fixed index.css name. Updated tailwind.config to work with Vite.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppWrapper from "./AppWrapper";
 import App from "./App.jsx";
-import "./input.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+  // * Set up to work with Vite
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   plugins: [],
 };
