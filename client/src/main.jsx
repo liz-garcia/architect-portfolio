30dbc41129dfee3b282e6a56051ffc2af36d32f8
liz-garcia <lizgarciamarin@gmail.com>
2024-08-23 14:01:48 +0200

    Fixed css import on main.jsx.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppWrapper from "./AppWrapper";
 import App from "./App.jsx";
-import "./main.css";
+import "./input.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
