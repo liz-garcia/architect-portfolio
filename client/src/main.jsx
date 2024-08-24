501faa16ad6b5dc8d6a3d18f14f5af5666c0206f
liz-garcia <lizgarciamarin@gmail.com>
2024-08-24 18:09:44 +0200

    Add utility classes to root on main.jsx.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,13 @@ import AppWrapper from "./AppWrapper";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Get the root element
+// Add TailwindCSS utility classes
+const rootElement = document.getElementById("root");
+rootElement.className = "bg-gray-100 p-4 text-center";
+
+// Render the React application
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     {/* AppWrapper for providers and contexts. */}
     <AppWrapper>
