16a6904fd86baf4581085afe4c9057de448d2086
liz-garcia <lizgarciamarin@gmail.com>
2024-08-26 19:14:30 +0200

    Imported new fonts. Customized sans font family.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   // * Set up to work with Vite
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // ! Extend Tailwind CSS default theme configuration
+      fontFamily: {
+        // --> Extend/change the default fonts for our project
+        // --> Extending the `sans` font family: by default Tailwind CSS sets the font family on the html element to match your configured sans font. So customizing `sans` will change the default font for your project.
+        sans: ["Jura", "sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
+
+// module.exports = {
+//   theme: {
+//     fontFamily: {
+//       'sans': ['ui-sans-serif', 'system-ui', ...],
+//       'serif': ['ui-serif', 'Georgia', ...],
+//       'mono': ['ui-monospace', 'SFMono-Regular', ...],
+//       'display': ['Oswald', ...],
+//       'body': ['"Open Sans"', ...],
+//     }
+//   }
+// }
