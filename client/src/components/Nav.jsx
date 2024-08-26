1e37b1630419415991e2359463dcff9bff37b446
liz-garcia <lizgarciamarin@gmail.com>
2024-08-26 12:05:14 +0200

    Reset styles and pages to start clean.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,16 +2,10 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="w-full border-b pb-2">
-      <ul className="m-auto flex w-fit space-x-8 text-zinc-400">
-        <Link to="/" className="hover:text-white hover:underline">
-          <li className="p-2">Home</li>
-        </Link>
-        <Link to="/user/list" className="hover:text-white hover:underline">
-          <li className="p-2">User List</li>
-        </Link>
-        <Link to="/user/create" className="hover:text-white hover:underline">
-          <li className="p-2">Create User</li>
+    <nav>
+      <ul>
+        <Link to="/">
+          <li>Home</li>
         </Link>
       </ul>
     </nav>
