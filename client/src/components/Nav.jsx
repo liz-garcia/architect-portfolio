60122c56ac943d8e57c64133b88ad98df5555471
liz-garcia <lizgarciamarin@gmail.com>
2024-08-28 11:28:27 +0200

    Nav initial style.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 
+// * Tailwind CSS utility classes
+const navStyle = "w-fit";
+const ulStyle = "flex space-x-4";
+const linkStyle = "px-4 py-2 hover:underline underline-offset-8";
+const liStyle = "";
+
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <Link to="/">
-          <li>Home</li>
+    <nav className={navStyle}>
+      <ul className={ulStyle}>
+        <Link to="/" className={linkStyle}>
+          <li className={liStyle}>About</li>
+        </Link>
+        <Link to="/" className={linkStyle}>
+          <li className={liStyle}>Portfolio</li>
+        </Link>
+        <Link to="/" className={linkStyle}>
+          <li className={liStyle}>Resume</li>
         </Link>
       </ul>
     </nav>
