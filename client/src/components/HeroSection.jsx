bae76b50c442f8bfcf4d0cfd71e7416c82fc9b73
liz-garcia <lizgarciamarin@gmail.com>
2024-09-01 12:33:40 +0200

    Changed tailwind congif file so our screens work with max-width breakpoints instead of min-width.

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -5,13 +5,13 @@ import heroImage2 from "/architectural-plan-2.png";
 import heroImage3 from "/architectural-plan-3.png";
 
 // Tailwind CSS utility classes
-const heroSectionStyle = "h-80dvh flex pl-16 items-center gap-8";
+const heroSectionStyle = "h-[85dvh] flex pl-16 items-center gap-8 sm:flex-col";
 const heroTitleStyle = "w-1/3 text-center";
 const h1Style = "font-serif font-bold text-5xl/none lg:text-7xl/[3.5rem] mb-4";
 const h2Style = "text-lg/5 py-2 mb-4";
 const heroImgContainerStyle = "m-auto w-2/3 h-full bg-no-repeat bg-left-top";
 const heroImgStyle =
-  "w-full h-full object-contain transition-opacity duration-1000 ease-in-out";
+  "w-full h-full object-cover transition-opacity duration-1000 ease-in-out";
 
 // HeroSection component
 function HeroSection() {
@@ -35,7 +35,7 @@ function HeroSection() {
   return (
     <>
       <section id="heroSection" className={heroSectionStyle}>
-        <div id="hero-title" className={heroTitleStyle}>
+        <div id="heroTitle" className={heroTitleStyle}>
           <h1 className={h1Style}>Architect Portfolio</h1>
           <h2 className={h2Style}>
             Rhina Aragón, senior architect based in Minnesota.
@@ -44,7 +44,7 @@ function HeroSection() {
         </div>
         <div id="hero-image-container" className={heroImgContainerStyle}>
           <img
-            id="hero-image"
+            id="heroImage"
             src={images[currentImageIndex]}
             alt={`Architectural Plan ${currentImageIndex + 1}`}
             className={`${heroImgStyle} ${fade ? "opacity-100" : "opacity-0"}`}
