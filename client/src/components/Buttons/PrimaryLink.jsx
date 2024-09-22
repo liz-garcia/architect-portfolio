8d71acfdc4dfa62a6e939dae017db6f1f3549357
liz-garcia <lizgarciamarin@gmail.com>
2024-09-22 23:01:06 +0200

    Fixed style for buttons.

--- client/src/components/Buttons/PrimaryLink.jsx
+++ client/src/components/Buttons/PrimaryLink.jsx
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 // Tailwind CSS utility classes
 const size = "w-fit";
-const text = "font-medium text-zinc-800 text-center uppercase text-sm/loose";
+const text = "font-normal text-zinc-800 text-center uppercase text-sm/loose";
 const layout = "block px-5 py-3 rounded-lg";
-const color = "bg-lime-200 border-2 border-lime-300 drop-shadow-md"; // Initial color before hover
+const color = "bg-lime-200 border border-lime-300 drop-shadow-md"; // Initial color before hover
 
 // Transition from solid slate to gradient, with smooth color movement
 const transitionColor = "hover:bg-green-300 hover:border-green-400";
 const transition =
   "hover:scale-105 transform transition ease-in-out duration-500";
-const transitionFont = "hover:font-medium hover:text-sm/loose";
+const transitionFont = "hover:font-normal hover:text-sm/loose";
 
 // Final PrimaryLink style
 const primaryLinkStyle = `${size} ${text} ${layout} ${color} ${transition} ${transitionFont} ${transitionColor}`;
