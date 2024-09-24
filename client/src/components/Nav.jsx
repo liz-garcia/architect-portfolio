cb0252dbf5dbec673db4798b20c471b9cb7a02b6
liz-garcia <lizgarciamarin@gmail.com>
2024-09-24 18:51:11 +0200

    Fixed Header for extra small screens.

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,7 +5,7 @@ const navStyle = "p-1";
 const ulStyle = "h-full flex space-x-4 sm:space-x-1 xs:space-x-4";
 const liStyle = "h-full w-20 xs:w-fit";
 const linkStyle =
-  "uppercase font-light size-full flex items-center justify-center text-center hover:underline underline-offset-8 text-sm xs:text-base xs:p-1 xs:pt-0";
+  "uppercase font-light size-full flex items-center justify-center text-center hover:underline underline-offset-8 text-sm xs:text-base xs:p-1";
 
 const Nav = () => {
   return (
