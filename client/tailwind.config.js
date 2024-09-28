912c7205783a73068c69583fdf1d6f7ec69cebc1
liz-garcia <lizgarciamarin@gmail.com>
2024-09-28 17:24:47 +0200

    Fixed comment.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -43,7 +43,7 @@ export default {
       // ! Extend to add specific tag for specific screen size (max-values)
       screens: {
         "h-667-w-375": { raw: "(max-height: 667px) and (max-width: 375px)" },
-        // ! Specific for small screens with portrait/landscape orientation
+        // ! Specific for 'xs' screens with portrait/landscape orientation
         "xs-portrait": {
           raw: "(max-width: 450px) and (orientation: portrait)",
         },
