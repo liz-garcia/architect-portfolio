764c11498697871eebb694577f302cab0d0c596e
liz-garcia <lizgarciamarin@gmail.com>
2024-10-05 21:02:46 +0200

    Implemented solutions for hadling our URL when navigating to or from About. Needs extra work.

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import HeroSection from "./HeroSection.jsx";
 import About from "../About/About.jsx"; // * Import About page
 
@@ -13,7 +13,37 @@ import About from "../About/About.jsx"; // * Import About page
 // Home page
 function Home() {
   const location = useLocation();
+  const navigate = useNavigate();
 
+  // TODO Explanatory comment here
+  useEffect(() => {
+    // Check if the user is on the homepage and the URL is not already "/"
+    if (location.pathname === "/home" || location.pathname === "/about") {
+      // Use history.replaceState to change the URL to "/" without reloading
+      window.history.replaceState({}, "", "/");
+    }
+  }, [location]); // This will run whenever the location changes
+
+  // TODO Explanatory comment here
+  useEffect(() => {
+    const handleScroll = () => {
+      // Check if the user has scrolled to the top
+      if (window.scrollY === 0 && location.pathname !== "/") {
+        // Change the URL to "/" without reloading the page
+        navigate("/", { replace: true });
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [location, navigate]); // Re-run if location or navigate changes
+
+  // TODO Explanatory comment here
   useEffect(() => {
     if (location.pathname === "/about") {
       const aboutSection = document.getElementById("about");
