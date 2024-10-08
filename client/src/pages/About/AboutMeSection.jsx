dc34b9b0e29855303c7fad316d0dcc67c4402d25
liz-garcia <lizgarciamarin@gmail.com>
2024-10-08 20:32:44 +0200

    Added responsive styles for Featured projects section.

--- client/src/pages/About/AboutMeSection.jsx
+++ client/src/pages/About/AboutMeSection.jsx
@@ -1,10 +1,11 @@
 import PrimaryLink from "../../components/Buttons/PrimaryLink.jsx";
 
-// * Tailwind CSS utility classes
+// * ***** Tailwind CSS utility classes *****
 const aboutMeContainer =
-  "3xl:h-1/2 3xl:flex 3xl:flex-col 3xl:items-center 3xl:justify-center";
+  "2xl:h-fit 3xl:h-1/2 3xl:flex 3xl:flex-col 3xl:items-center 3xl:justify-center";
+// * aboutMe Section element
 const aboutMeStyle =
-  "px-28 pt-16 pb-10 md:pb-8 sm:pb-16 flex md:flex-col lg:flex-row gap-4 md:gap-2 sm:gap-4 justify-center sm:h-dvh sm:px-16 sm:mx-2 xs:h-svh xs:px-10 xs:gap-2 md:mb-4 2xs:px-8";
+  "h-fit px-28 pt-16 pb-10 lg:px-20 md:pb-8 sm:pb-16 flex md:flex-col lg:flex-row gap-4 md:gap-2 sm:gap-4 justify-center sm:h-dvh sm:px-16 sm:mx-2 xs:h-svh xs:px-10 xs:gap-2 md:mb-4 2xs:px-8";
 // * aboutMeTitle
 const aboutMeTitleStyle = "md:flex-none flex-1 w-1/3 md:w-auto";
 const h2Style = "font-bold font-serif text-4xl xs:text-[2.1rem] 3xl:text-6xl";
@@ -12,7 +13,7 @@ const h2Style = "font-bold font-serif text-4xl xs:text-[2.1rem] 3xl:text-6xl";
 const aboutMeTextStyle =
   "md:flex-none flex flex-col gap-1 w-2/3 md:w-auto pt-2 pl-10 pr-18 md:pl-0 sm:pt-0 3xl:pl-16";
 const h3Style =
-  "font-light text-2xl text-zinc-800 italic md:text-xl xs:text-2xl sm:mb-1 h-667-w-375:text-xl 3xl:text-4xl 3xl:mt-2";
+  "font-light text-2xl text-zinc-800 italic lg:text-xl xs:text-2xl sm:mb-1 h-667-w-375:text-xl 3xl:text-4xl 3xl:mt-2";
 const elevatorPitchStyle =
   "text-justify my-1 sm:mb-4 h-667-w-375:text-base 2xs:text-base 3xl:text-2xl/loose 3xl:pt-4";
 const dividerStyle1 =
