02ef1a3074d306d69e2900e329904c72599408ff
liz-garcia <lizgarciamarin@gmail.com>
2024-10-11 17:32:19 +0200

    Fixed sizing for elements to balance ratio for images in FeaturedProjects section.

--- client/src/pages/About/AboutMeSection.jsx
+++ client/src/pages/About/AboutMeSection.jsx
@@ -6,19 +6,19 @@ const aboutMeContainer =
   "2xl:h-fit 3xl:h-1/2 3xl:flex 3xl:flex-col 3xl:items-center 3xl:justify-center";
 // * aboutMe Section element
 const aboutMeStyle =
-  "h-fit px-28 pt-16 pb-10 lg:px-20 md:pb-4 lg:pb-8 sm:pb-16 flex lg:flex-col 2xl:flex-row gap-4 lg:gap-2 sm:gap-4 justify-center sm:h-dvh sm:px-16 sm:mx-2 xs:h-svh xs:px-10 xs:gap-2 md:mb-4 2xs:px-8";
+  "h-fit px-28 lg:px-18 md:px-12 xl:px-16 pt-16 sm:pt-12 md:pt-8 lg:pt-10 pb-10 lg:px-12 2xl:px-16 md:pb-4 xl:pb-4 2xl:pb-5 sm:pb-16 flex lg:flex-col 2xl:flex-row gap-4 lg:gap-0 lg:gap-2 sm:gap-2 justify-center sm:h-dvh sm:px-16 sm:mx-2 xs:h-svh xs:px-10 xs:gap-2 2xs:px-8";
 // * aboutMeTitle
-const aboutMeTitleStyle = "lg:flex-none flex-1 w-1/3 lg:w-auto";
+const aboutMeTitleStyle = "lg:flex-none flex-1 w-1/3 lg:w-auto xl:w-1/5";
 const h2Style = "font-bold font-serif text-4xl xs:text-[2.1rem] 3xl:text-6xl";
 // * aboutMeText
 const aboutMeTextStyle =
-  "lg:flex-none flex flex-col gap-1 w-2/3 lg:w-auto pt-2 pl-4 pr-18 lg:pl-0 sm:pt-0 3xl:pl-16";
+  "lg:flex-none flex flex-col gap-1 w-2/3 lg:w-auto pt-2 pl-4 pr-18 lg:pl-0 sm:pt-0 3xl:pl-16 xl:w-4/5";
 const h3Style =
   "font-light text-2xl text-zinc-800 italic lg:text-xl xs:text-2xl sm:mb-1 h-667-w-375:text-xl 3xl:text-4xl 3xl:mt-2";
 const elevatorPitchStyle =
-  "text-justify my-1 sm:mb-4 lg:pb-0 2xl:pb-1 h-667-w-375:text-base 2xs:text-base 3xl:text-2xl/loose 3xl:pt-4";
+  "text-justify my-1 md:mb-0 sm:mb-4 lg:pb-0 2xl:pb-1 2xl:leading-relaxed h-667-w-375:text-base 2xs:text-base 3xl:text-2xl/loose 3xl:pt-4";
 const dividerStyle1 =
-  "absolute -right-[56px] w-2/3 h-12 border-b border-zinc-300 lg:hidden 3xl:hidden";
+  "absolute -right-[56px] xl:-right-[56px] 2xl:-right-[36px] xl:w-4/5 w-2/3 h-12 border-b border-zinc-300 lg:hidden 3xl:hidden";
 const buttonContainerStyle =
   "flex justify-between gap-4 sm:py-0 md:py-2 xl:py-1 2xl:py-0 3xl:py-4 3xl:gap-8 items-center";
 const dividerStyle2 = "flex-1 border-b border-zinc-300";
@@ -65,7 +65,7 @@ const AboutMeSection = () => {
             <div
               className={
                 isSmallScreen
-                  ? "flex flex-wrap-reverse gap-2 xs:place-content-center h-667-w-375:scale-[0.8] h-667-w-375:flex-nowrap"
+                  ? "flex gap-2 xs:scale-90 xs:flex-row-reverse xs:place-content-center 2xs:scale-90 h-667-w-375:scale-[0.8] h-667-w-375:flex-nowrap"
                   : buttonContainerStyle
               }
             >
