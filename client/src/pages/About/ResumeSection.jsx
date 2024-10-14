c2fda917c3c5ffafa04251635509d135350dd6b1
liz-garcia <lizgarciamarin@gmail.com>
2024-10-14 12:42:30 +0200

    Fixed Image component styles. Placed main elements for ResumeSction layout.

--- client/src/pages/About/ResumeSection.jsx
+++ client/src/pages/About/ResumeSection.jsx
@@ -1,4 +1,9 @@
-const resumeSectionStyle = "h-screen grid grid-cols-3 grid-rows-2 gap-4 p-16";
+import resumePicture1 from "../../assets/resumePictures/resumePicture1.png";
+import resumePicture2 from "../../assets/resumePictures/resumePicture2.png";
+import Image from "../../components/Image.jsx";
+
+const resumeSectionStyle =
+  "h-screen grid grid-cols-3 grid-rows-[1fr_2fr] gap-4 p-16";
 const resumeSubsection = "bg-zinc-400";
 const resumePictures = "bg-lime-200";
 const largePicture = "col-span-2";
@@ -7,6 +12,7 @@ const ResumeSection = () => {
   return (
     <>
       <section id="resume" className={resumeSectionStyle}>
+        {/* Texts for ResumeSection */}
         <div id="education" className={resumeSubsection}>
           <h1>Education</h1>
         </div>
@@ -16,11 +22,16 @@ const ResumeSection = () => {
         <div id="softwareSkills" className={resumeSubsection}>
           <h1>Software skills</h1>
         </div>
-        <div id="sectionPicture1" className={resumePictures}></div>
+        {/* Pictures for ResumeSection */}
+        <div id="resumePicture1" className={resumePictures}>
+          <Image src={resumePicture1} altText="resumePicture" />
+        </div>
         <div
-          id="sectionPicture2"
+          id="resumePicture2"
           className={`${resumePictures} ${largePicture}`}
-        ></div>
+        >
+          <Image src={resumePicture2} altText="resumePicture" />
+        </div>
       </section>
     </>
   );
