cffde9bf7df265d9e89f28fe22d805ce4803048a
liz-garcia <lizgarciamarin@gmail.com>
2024-10-16 18:36:29 +0200

    Fixed styles for 2xl, xl, and lg screens.

--- client/src/pages/About/ResumeSection.jsx
+++ client/src/pages/About/ResumeSection.jsx
@@ -4,18 +4,25 @@ import Image from "../../components/Image.jsx";
 import PrimaryLink from "../../components/Buttons/PrimaryLink.jsx";
 
 const resumeSectionStyle =
-  "h-screen grid grid-cols-3 grid-rows-[1fr_2fr] gap-2 p-12 3xl:p-20";
-const resumeSubsection = "p-2 flex flex-col gap-2 3xl:p-8 3xl:gap-4";
-const h1Style = "font-serif font-bold text-3xl pb-2 3xl:text-5xl";
-const infoContainer = "grid grid-rows-1 grid-cols-[1fr_2fr] gap-2";
-const h2Style = "font-bold leading-tight 3xl:text-2xl";
-const itemTitle = "leading-tight 3xl:text-2xl";
-const itemDetail = "text-base italic leading-tight 3xl:text-xl";
-const resumePictures = "";
-const largePicture = "col-span-2";
+  "h-screen grid grid-cols-[1fr_2fr_2fr] grid-rows-[1fr_2fr] gap-4 lg:gap-2 px-16 py-12 pb-4 3xl:p-20 md:bg-zinc-200 sm:bg-red-100";
+const resumeSubsection =
+  "p-2 pt-0 flex flex-col gap-2 3xl:p-8 3xl:gap-4 xl:text-base lg:gap-3";
+const h1Style =
+  "font-serif font-bold text-3xl pb-2 3xl:text-5xl border-b mb-2 lg:pb-1 lg:mb-1 lg:text-[1.75rem] md:text-2xl";
+const infoContainer =
+  "leading-tight grid grid-rows-1 grid-cols-[1fr_2fr] gap-2 lg:flex lg:flex-col lg:gap-1";
+const h2Style = "3xl:text-2xl";
+const itemTitle = "font-semibold 3xl:text-2xl";
+const itemDetail = "text-base italic xl:text-sm 3xl:text-xl";
+const educationStyle = "2xl:flex 2xl:flex-col 2xl:gap-1";
+const workExperienceStyle = "";
 const softwareList =
-  "grid grid-cols-3 gap-2 text-center leading-tight 3xl:text-2xl 3xl:gap-4";
-const buttonContainer = "m-auto";
+  "leading-tight flex flex-wrap gap-1 text-center justify-center 3xl:text-2xl 3xl:gap-4";
+const dotStyle =
+  "w-1 h-1 bg-lime-400 rounded place-self-center mx-2 last:hidden";
+const buttonContainer = "m-auto xl:scale-95";
+const smallPicture = "";
+const largePicture = "col-span-2";
 
 const ResumeSection = () => {
   return (
@@ -24,7 +31,7 @@ const ResumeSection = () => {
         {/* Texts for ResumeSection */}
         <div id="education" className={resumeSubsection}>
           <h1 className={h1Style}>Education</h1>
-          <div className={infoContainer}>
+          <div className={`${infoContainer} ${educationStyle}`}>
             <h2 className={h2Style}>2010 - 2015</h2>
             <div>
               <p className={itemTitle}>Bachelor of Architecture</p>
@@ -34,21 +41,21 @@ const ResumeSection = () => {
         </div>
         <div id="workExperience" className={resumeSubsection}>
           <h1 className={h1Style}>Work experience</h1>
-          <div className={infoContainer}>
+          <div className={`${infoContainer} ${workExperienceStyle}`}>
             <h2 className={h2Style}>2016 - 2017</h2>
             <div>
               <p className={itemTitle}>Lead Architect</p>
               <p className={itemDetail}>Daibutsu Aladdin Casino Company</p>
             </div>
           </div>
-          <div className={infoContainer}>
+          <div className={`${infoContainer} ${workExperienceStyle}`}>
             <h2 className={h2Style}>2017 - 2023</h2>
             <div>
               <p className={itemTitle}>Estimator</p>
               <p className={itemDetail}>Nica Electronic Services NICAES</p>
             </div>
           </div>
-          <div className={infoContainer}>
+          <div className={`${infoContainer} ${workExperienceStyle}`}>
             <h2 className={h2Style}>2017 - Present</h2>
             <div>
               <p className={itemTitle}>Freelance Architect</p>
@@ -60,26 +67,30 @@ const ResumeSection = () => {
           <h1 className={h1Style}>Software skills</h1>
           <ul className={softwareList}>
             <li>AutoCAD</li>
+            <div className={dotStyle} />
             <li>Revit</li>
+            <div className={dotStyle} />
             <li>SketchUp</li>
+            <div className={dotStyle} />
             <li>Planswift</li>
+            <div className={dotStyle} />
             <li>Lumion</li>
+            <div className={dotStyle} />
             <li>Project</li>
+            <div className={dotStyle} />
             <li>Adobe Photoshop</li>
+            <div className={dotStyle} />
           </ul>
           <div className={buttonContainer}>
             <PrimaryLink to="/resume">View Full Resume PDF</PrimaryLink>
           </div>
         </div>
         {/* Pictures for ResumeSection */}
-        <div id="resumePicture1" className={resumePictures}>
-          <Image src={resumePicture1} altText="resumePicture" />
-        </div>
-        <div
-          id="resumePicture2"
-          className={`${resumePictures} ${largePicture}`}
-        >
+        <div id="resumePicture2" className={largePicture}>
           <Image src={resumePicture2} altText="resumePicture" />
+        </div>
+        <div id="resumePicture1" className={smallPicture}>
+          <Image src={resumePicture1} altText="resumePicture" />
         </div>
       </section>
     </>
