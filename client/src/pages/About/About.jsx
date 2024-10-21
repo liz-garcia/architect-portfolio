cb61c344973a38dfcedef39d8902b5c0d37f9a3b
liz-garcia <lizgarciamarin@gmail.com>
2024-10-21 15:52:45 +0200

    Adjusted responsive items for 'h-1180-1550' screen size.

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -4,7 +4,7 @@ import ResumeSection from "./ResumeSection.jsx";
 
 // Tailwind CSS utility classes
 const aboutStyle =
-  "w-screen relative overflow-hidden xs-landscape:block h-568-w-320:mt-4";
+  "w-screen relative overflow-hidden xs-landscape:block h-568-w-320:mt-4 3xl:pt-4 h-1180-1550:mt-4";
 const presentationStyle =
   "h-screen flex flex-col sm:h-fit xs-landscape:min-h-fit";
 
--- client/src/pages/About/FeaturedProjectsSection.jsx
+++ client/src/pages/About/FeaturedProjectsSection.jsx
@@ -5,7 +5,7 @@ import project3 from "../../assets/projects/project3.png";
 
 // Tailwind CSS utility classes
 const featuredProjectsStyle =
-  "flex-1 p-14 bg-zinc-900 text-white h-2/3 lg:h-1/2 xl:py-10 lg:px-0 lg:gap-4 md:h-auto sm:flex-none sm:h-screen flex justify-center gap-8 sm:flex-col 3xl:h-1/2 3xl:gap-12 3xl:py-16 sm:p-10 md:py-8 md:px-0 xs:p-10 lg-md-portrait:p-16 lg-md-portrait:flex lg-md-portrait:flex-col lg-md-portrait:gap-8 h-1180-1550:h-2/3 h-1180-1550:p-24 xs-landscape:h-lvh xs-landscape:min-h-fit h-568-w-320-landscape:flex-row xs-landscape:place-content-center h-568-w-320-landscape:p-8 h-568-w-320-landscape:place-content-center";
+  "flex-1 p-14 bg-zinc-900 text-white h-2/3 lg:h-1/2 xl:py-10 lg:px-0 lg:gap-4 md:h-auto sm:flex-none sm:h-screen flex justify-center gap-8 sm:flex-col 3xl:h-1/2 3xl:gap-12 3xl:py-16 sm:p-10 md:py-8 md:px-0 xs:p-10 lg-md-portrait:p-16 lg-md-portrait:flex lg-md-portrait:flex-col lg-md-portrait:gap-8 h-1180-1550:h-2/3 h-1180-1550:p-24 xs-landscape:h-lvh xs-landscape:min-h-fit h-568-w-320-landscape:flex-row xs-landscape:place-content-center h-568-w-320-landscape:p-8 h-568-w-320-landscape:place-content-center h-1180-1550:gap-10 h-1180-1550:py-16";
 // Project container style
 const imageContainerStyle =
   "3xl:w-3/12 lg:w-3/12 2xl:w-4/12 mb-2 rounded-xl overflow-hidden sm:size-full lg-md-portrait:m-0 lg-md-portrait:p-0 lg-md-portrait:w-auto lg-md-portrait:h-[32%] xs-landscape:h-[80vh]";
