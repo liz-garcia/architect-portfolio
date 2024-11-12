15291d42f78a15049c11c5af1f41a4e2374f559a
liz-garcia <lizgarciamarin@gmail.com>
2024-11-12 19:37:27 +0100

    Added extra rules for style on portrait and landscape.

--- client/src/pages/Home/ContactSection.jsx
+++ client/src/pages/Home/ContactSection.jsx
@@ -3,10 +3,10 @@ const ContactSection = () => {
     <>
       <section
         id="contact"
-        className="flex h-screen items-center border-b-8 border-lime-200 bg-zinc-900 h-1260-w-1024:h-[40vh]"
+        className="mt-8 flex h-screen items-center border-b-8 border-lime-200 bg-zinc-900 h-1260-w-1024:h-[40vh]"
       >
-        <div className="mx-auto my-auto flex h-5/6 w-4/6 flex-col place-content-stretch gap-8 portrait:w-4/6">
-          <div className="basis-1/2 rounded-2xl bg-white"></div>
+        <div className="mx-auto my-auto flex flex-col place-content-stretch gap-8 lg:gap-6 portrait:h-5/6 portrait:w-4/6 landscape:h-4/6 landscape:w-4/5 landscape:flex-row">
+          <div className="basis-1/2 rounded-2xl bg-white landscape:basis-2/5"></div>
           <div className="basis-full rounded-2xl bg-white"></div>
         </div>
       </section>
