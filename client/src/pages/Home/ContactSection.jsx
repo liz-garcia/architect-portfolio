db76d8f11846da123caf1431c6b54117639f1987
liz-garcia <lizgarciamarin@gmail.com>
2024-11-13 11:21:00 +0100

    Modified mt for contcat section.

--- client/src/pages/Home/ContactSection.jsx
+++ client/src/pages/Home/ContactSection.jsx
@@ -3,7 +3,7 @@ const ContactSection = () => {
     <>
       <section
         id="contact"
-        className="mt-8 flex h-screen items-center border-b-8 border-lime-200 bg-zinc-900 h-1260-w-1024:h-[40vh]"
+        className="mt-4 flex h-screen items-center border-b-8 border-lime-200 bg-zinc-900 h-1260-w-1024:h-[40vh]"
       >
         <div className="mx-auto my-auto flex flex-col place-content-stretch gap-8 lg:gap-6 portrait:h-5/6 portrait:w-4/6 landscape:h-4/6 landscape:w-4/5 landscape:flex-row">
           <div className="basis-1/2 rounded-2xl bg-white landscape:basis-2/5"></div>
