d9246547f16a334a463b7553e8fa0109c9c85371
liz-garcia <lizgarciamarin@gmail.com>
2024-11-18 12:18:58 +0100

    Created LoadingSpinner.

--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import LoadingSpinner from "./components/LoadingSpinner.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 // * Lazy loading
@@ -12,34 +13,7 @@ import CreateUser from "./pages/Admin/CreateUser.jsx";
 
 const AppRouter = () => {
   return (
-    <Suspense
-      fallback={
-        <div className="flex h-[90vh] w-[100vw] place-content-center">
-          <div className="mx-auto my-auto">
-            <svg
-              className="h-12 w-12 animate-spin text-zinc-700"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-              ></path>
-            </svg>
-          </div>
-        </div>
-      }
-    >
+    <Suspense fallback={<LoadingSpinner height="h-[50vh]" width="w-full" />}>
       <Routes>
         {/* Home page includes the About page as AboutSection */}
         <Route path="/" element={<Home />} />
---
+++ client/src/components/LoadingSpinner.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+
+const LoadingSpinner = (height, width) => {
+  return (
+    <div
+      id="loadingSpinner"
+      className={`flex ${height} ${width} place-content-center`}
+    >
+      <div className="mx-auto my-auto">
+        <svg
+          className="h-12 w-12 animate-spin text-zinc-700"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+// Prop-types validation
+LoadingSpinner.propTypes = {
+  height: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+};
+
+export default LoadingSpinner;
