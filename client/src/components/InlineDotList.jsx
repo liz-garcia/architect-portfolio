4c4ea33aed558d402bd8a6d180b174b2d145ce98
liz-garcia <lizgarciamarin@gmail.com>
2024-12-09 09:20:27 +0100

    Created InlineDotList component.

---
+++ client/src/components/InlineDotList.jsx
@@ -0,0 +1,29 @@
+// * InlineDotList style for subsection 'Skills' at Resume section on Homepage
+import PropTypes from "prop-types";
+
+// Tailwind CSS styles
+const softwareList =
+  "leading-tight flex flex-wrap gap-2 text-center justify-center lg:p-1 lg:pt-0 3xl:text-2xl 3xl:gap-4 sm:px-0 xs:pt-1 h-568-w-320-landscape:text-sm h-568-w-320-landscape:py-2 h-658-w-320-landscape-extra:text-sm h-667-w-375-landscape-extra:text-sm";
+const dotStyle = "w-1 h-1 bg-lime-400 rounded place-self-center ml-2";
+
+// InlineDotList component
+const InlineDotList = ({ data }) => {
+  return (
+    <ul className={softwareList}>
+      {data.map((item, index) => (
+        <li key={item} className="flex items-center">
+          <span>{item}</span>
+          {index < data.length - 1 && <div className={dotStyle} />}{" "}
+          {/* Render dot only if not the last item */}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+// Prop-types validation
+InlineDotList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default InlineDotList;
--- client/src/pages/About/ResumeSection.jsx
+++ client/src/pages/About/ResumeSection.jsx
@@ -4,6 +4,7 @@ import resumePicture2 from "../../assets/resumePictures/resumePicture2.png";
 import Image from "../../components/Image.jsx";
 import PrimaryLink from "../../components/Buttons/PrimaryLink.jsx";
 import InfoContainer from "../../components/InfoContainer.jsx";
+import InlineDotList from "../../components/InlineDotList.jsx";
 
 const resumeSectionStyle =
   "min-h-fit h-screen grid grid-cols-3 xl:grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_2fr] lg:grid-rows-[2fr_1fr_3fr] lg:grid-cols-[1fr_1fr_1fr] gap-4 xl:gap-2 px-16 py-12 pb-4 lg:px-12 lg:pt-8 lg:pb-2 lg:portrait:grid-rows-[auto_auto_1fr] lg:portrait:px-16 lg:portrait:py-16 lg:portrait:gap-4 3xl:p-20 md:portrait:gap-4 md:portrait:px-10 md:portrait:pt-10 md:portrait:pb-0 sm:portrait:min-h-fit sm:portrait:h-screen sm:flex sm:flex-col sm:portrait:flex sm:portrait:flex-col sm:portrait:px-12 sm:portrait:gap-4 xs:portrait:gap-4 sm:portrait:place-content-center sm:portrait:pt-8 sm:portrait:pb-8 xs:portrait:py-8 xs:portrait:px-8 xs-landscape:px-10 xs-landscape:pb-8 h-1260-w-1024:h-[50vh] ipad-surface-pro:portrait:h-[50vh] ipad-surface-pro:portrait:gap-2 ipad-surface-pro:portrait:p-12 ipad-surface-pro:portrait:pb-2 h-568-w-320-landscape:grid-rows-[2fr_1fr] h-568-w-320-landscape:landscape:py-6 h-568-w-320-landscape:landscape:px-8 h-667-w-375-landscape-extra:gap-1 h-667-w-375-landscape-extra:py-6 h-667-w-375-landscape-extra:grid-rows-[2fr_1fr] 2xs:portrait:gap-2 ipad-surface-pro-landscape:pt-20 ipad-surface-pro-landscape:pb-12";
@@ -17,10 +18,6 @@ const workExperience =
   "lg:col-start-1 lg:col-span-2 lg:row-start-1 lg:row-span-1 lg:pr-8 md:pr-4 sm:order-first sm:pr-0 sm:px-0";
 const softwareSkills =
   "lg:col-start-3 lg:row-start-1 lg:col-span-1 lg:row-span-1 lg:px-1 lg:pb-1 lg:text-[0.95rem] lg:portrait:px-0 sm:order-3 sm:portrait:order-3 sm:portrait:pr-0 sm:portrait:px-0";
-const softwareList =
-  "leading-tight flex flex-wrap gap-1 text-center justify-center lg:p-1 lg:pt-0 3xl:text-2xl 3xl:gap-4 sm:px-0 xs:pt-1 h-568-w-320-landscape:text-sm h-568-w-320-landscape:py-2 h-658-w-320-landscape-extra:text-sm h-667-w-375-landscape-extra:text-sm";
-const dotStyle =
-  "w-1 h-1 bg-lime-400 rounded place-self-center mx-2 last:hidden lg:mx-1 sm:mx-1 xs:mx-1";
 const buttonContainer =
   "mx-auto my-auto xl:scale-95 lg:scale-[0.85] lg:portrait:mt-2 sm:portrait:mt-1 md:landscape:mt-2 xs-landscape:landscape:mt-0 sm:scale-95 2xs:scale-90 h-568-w-320-landscape:scale-90";
 const pictures = "2xl:pt-2 md:portrait:pt-0 px-2";
@@ -31,8 +28,9 @@ const largePicture =
 
 const ResumeSection = ({ data }) => {
   // * Get an array of objects for every subsection
-  const educationItems = data ? data.educationHighlights : null;
-  const workExperienceItems = data ? data.workExperienceHighlights : null;
+  const educationItems = data ? data.educationHighlights : null; // array of objects
+  const workExperienceItems = data ? data.workExperienceHighlights : null; // array of objects
+  const skillsItems = data ? data.skillsHighlights : null; // array of strings
 
   return (
     <>
@@ -70,22 +68,10 @@ const ResumeSection = ({ data }) => {
           className={`${resumeSubsection} ${softwareSkills}`}
         >
           <h1 className={h1Style}>Software skills</h1>
-          <ul className={softwareList}>
-            <li>AutoCAD</li>
-            <div className={dotStyle} />
-            <li>Revit</li>
-            <div className={dotStyle} />
-            <li>SketchUp</li>
-            <div className={dotStyle} />
-            <li>Planswift</li>
-            <div className={dotStyle} />
-            <li>Lumion</li>
-            <div className={dotStyle} />
-            <li>Project</li>
-            <div className={dotStyle} />
-            <li>Adobe Photoshop</li>
-            <div className={dotStyle} />
-          </ul>
+          {/* Render InlineDotList with the array 'skillsItems' */}
+          <InlineDotList data={skillsItems} />
+
+          {/* Button for accessing Full Resume */}
           <div className={buttonContainer}>
             <PrimaryLink to="/resume">View Full Resume</PrimaryLink>
           </div>
