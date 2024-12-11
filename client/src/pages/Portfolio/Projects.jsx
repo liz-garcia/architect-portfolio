4fdbf27ad2f9632f4d3258556bdd8b1b6ac7e4e6
liz-garcia <lizgarciamarin@gmail.com>
2024-12-11 11:52:39 +0100

    Run prettier.

--- client/src/pages/Portfolio/Projects.jsx
+++ client/src/pages/Portfolio/Projects.jsx
@@ -6,7 +6,7 @@ import project3 from "../../assets/projects/project3.png";
 const projectsPageStyle = "bg-zinc-900 animate-fadeIn w-screen h-[75vh]";
 
 const Projects = () => {
-  const images = [project1, project2, project3, project3, project1, project2,];
+  const images = [project1, project2, project3, project3, project1, project2];
 
   return (
     <div id="projectsPage" className={projectsPageStyle}>
