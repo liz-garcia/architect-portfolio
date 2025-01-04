4f47c33fddd21de476abe1cd82356c235040b061
liz-garcia <lizgarciamarin@gmail.com>
2025-01-04 18:27:56 +0100

    Fixed footer bottom padding for 3xl screens.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 // Tailwind CSS utility classes
 const footerStyle =
-  "h-[12dvh] w-full text-xs text-zinc-400 bg-zinc-950 p-12 px-16 pb-16 flex justify-between items-stretch md:flex-col-reverse md:px-8 md:py-14 md:gap-4 md:items-center md:place-content-center xs:h-[16dvh] xs:py-16 3xl:text-lg 3xl:px-20";
+  "h-[12dvh] w-full text-xs text-zinc-400 bg-zinc-950 p-12 px-16 pb-16 flex justify-between items-stretch md:flex-col-reverse md:px-8 md:py-14 md:gap-4 md:items-center md:place-content-center xs:h-[16dvh] xs:py-16 3xl:text-lg 3xl:px-20 3xl:pb-20";
 const copyrightStyle = "";
 const linksListStyle = "";
 const ulStyle =
