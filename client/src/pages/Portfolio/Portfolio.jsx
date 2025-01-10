faf8c412d3444cc3d1cf4628027736d9ac42b23e
liz-garcia <lizgarciamarin@gmail.com>
2025-01-10 17:09:38 +0100

    Fixed bg colorto white.

--- client/src/pages/Portfolio/Portfolio.jsx
+++ client/src/pages/Portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import project1 from "../../assets/projects/project1.png";
 import project2 from "../../assets/projects/project2.png";
 import project3 from "../../assets/projects/project3.png";
 
-const portfolioPageStyle = "relative bg-red-200 animate-fadeIn w-full h-full";
+const portfolioPageStyle = "relative bg-white animate-fadeIn w-full h-full";
 
 const Portfolio = () => {
   const images = [project1, project2, project3, project3];
