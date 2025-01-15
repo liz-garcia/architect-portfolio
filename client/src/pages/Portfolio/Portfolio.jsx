3edda5afcec229e42f7df2dc3cfa7d3d191aa725
liz-garcia <lizgarciamarin@gmail.com>
2025-01-15 16:43:25 +0100

    Minor adjustments to Portfolio gallery and Footer responsive design.

--- client/src/pages/Portfolio/Portfolio.jsx
+++ client/src/pages/Portfolio/Portfolio.jsx
@@ -2,8 +2,11 @@ import usePortfolio from "../../contexts/usePortfolio.js";
 import ImageGrid from "../../components/Galleries/ImageGrid.jsx";
 
 const portfolioPageStyle =
-  "relative bg-gradient-to-br from-zinc-900 to-zinc-950 animate-fadeInUp w-full h-full";
+  "bg-gradient-to-br from-zinc-900 to-zinc-950 animate-fadeInUp w-full h-full";
 
+// * Maximal 9 projects - Otherwise, it is necessary to adjust de ImageGrid height for lg to 2xl and 3xl screens.
+
+// Portfolio Gallery component
 const Portfolio = () => {
   const { portfolio } = usePortfolio();
 
